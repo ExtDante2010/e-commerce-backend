36153b2dd36b5a5408b87fd10054aa1cce43b7ce
ExtDante2010 <aldairlugofuentes@hotmail.com>
2023-03-14 11:33:58 -0500

    add like and dislike functionality

--- controller/blogController.js
+++ controller/blogController.js
@@ -20,6 +20,7 @@ export const createBlog = asyncHandlers(async (req, res) => {
 
 export const updateBlog = asyncHandlers(async (req, res) => {
   const { id } = req.params;
+  validateMoongoId(id);
   try {
     const blogUpdate = await Blog.findOneAndUpdate(id, req.body, {
       new: true,
@@ -34,8 +35,16 @@ export const updateBlog = asyncHandlers(async (req, res) => {
 
 export const searchBlog = asyncHandlers(async (req, res) => {
   const { id } = req.params;
+  validateMoongoId(id);
   try {
-    const findBlog = await Blog.findById(id);
+    const findBlog = await Blog.findById(id)
+      .populate("likes")
+      .populate("dislikes");
+    const updateView = await Blog.findByIdAndUpdate(
+      id,
+      { $inc: { numViews: 1 } },
+      { new: true }
+    );
     res.json(findBlog);
   } catch {
     throw new Error("Blog not found");
@@ -57,10 +66,115 @@ export const searchAllBlogs = asyncHandlers(async (req, res) => {
 
 export const deleteBlog = asyncHandlers(async (req, res) => {
   const { id } = req.params;
+  validateMoongoId(id);
   try {
     const deleted = await Blog.findOneAndDelete(id);
     res.json({ deleted });
   } catch {
     throw new Error("Blog not found");
+  }
+});
+
+// LIKE BLOG //
+
+export const likeBlog = asyncHandlers(async (req, res) => {
+  const { blogId } = req.body;
+  validateMoongoId(blogId);
+
+  //-- FIND THE BLOG WITCH YOU WANT TO BE LIKE --//
+  const blog = await Blog.findById(blogId);
+
+  //- FIND THE LOGIN USER --//
+  const loginUserId = req?.user?._id;
+
+  //- FIND IF THE USER HAS LIKED THE LOG --//
+  const isLike = blog?.isLike;
+
+  //-FIND IF THE USER HAS DISLIKED THE BLOG -//
+  const disLiked = blog?.dislikes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (disLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDislike: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+  if (isLike) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLike: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: loginUserId },
+        isLike: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+});
+
+export const dislikeBlog = asyncHandlers(async (req, res) => {
+  const { blogId } = req.body;
+  validateMoongoId(blogId);
+
+  //-- FIND THE BLOG WITCH YOU WANT TO BE LIKE --//
+  const blog = await Blog.findById(blogId);
+
+  //- FIND THE LOGIN USER --//
+  const loginUserId = req?.user?._id;
+
+  //- FIND IF THE USER HAS LIKED THE LOG --//
+  const isDislike = blog?.isDislike;
+
+  //-FIND IF THE USER HAS DISLIKED THE BLOG -//
+  const Liked = blog?.likes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (Liked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLike: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+  if (isDislike) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDislike: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { dislikes: loginUserId },
+        isDislike: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
   }
 });
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -2,6 +2,8 @@ import Express from "express";
 import {
   createBlog,
   deleteBlog,
+  dislikeBlog,
+  likeBlog,
   searchAllBlogs,
   searchBlog,
   updateBlog,
@@ -12,8 +14,11 @@ export const router = Express.Router();
 
 // BLOG //
 
-router.post("/", authMidellware, isAdmin, createBlog);
-router.put("/update-blog/:id", authMidellware, isAdmin, updateBlog);
-router.get("/search-blog/:id", searchBlog);
-router.get("/all-blogs", searchAllBlogs);
-router.delete("/delete-blog/:id", deleteBlog);
+router.post("/", authMidellware, isAdmin, createBlog); // CREATE BLOG //
+router.put("/likes", authMidellware, likeBlog); // LIKE A BLOG //
+router.put("/dislikes", authMidellware, dislikeBlog); // DISLIKE A BLOG //
+
+router.put("/update-blog/:id", authMidellware, isAdmin, updateBlog); //UPDATE BLOG //
+router.get("/search-blog/:id", searchBlog); // SEARCH A BLOG //
+router.get("/all-blogs", searchAllBlogs); // SEARCH ALL BLOG //
+router.delete("/delete-blog/:id", authMidellware, isAdmin, deleteBlog); // DELETE A BLOG //
