62700928ae304472bbba9b465e43ee92bdb4fb2c
ExtDante2010 <aldairlugofuentes@hotmail.com>
2023-03-15 15:58:17 -0500

    add seccion category 1.0

--- controller/categotyController.js
+++ controller/categotyController.js
@@ -2,11 +2,61 @@ import expressAsyncHandler from "express-async-handler";
 import Category from "../models/categoryModel.js";
 import { validateMoongoId } from "../utils/validateMongodb.js";
 
+//CREATE CATEGORY //
+
 export const createCategory = expressAsyncHandler(async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
     res.json({ newCategory });
   } catch {
     throw new Error("CATEGORY IS CERATED");
+  }
+});
+
+// UPDATE CATEGORY //
+
+export const updateCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const update = await Category.findOneAndUpdate(id, req.body, { new: true });
+    res.json(update);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// SEARCH ALL CATEGORY //
+
+export const allCategory = expressAsyncHandler(async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.json(category);
+  } catch {
+    throw new Error();
+  }
+});
+
+// SEARCH A CATEGORY //
+
+export const getCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const category = await Category.findById(id);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// DELETE CATEGORY //
+
+export const deleteCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const categoryDelete = await Category.findOneAndDelete(id);
+    res.json({ categoryDelete });
+  } catch (error) {
+    throw new Error(error);
   }
 });
--- routes/categotyRoute.js
+++ routes/categotyRoute.js
@@ -1,7 +1,17 @@
 import Express from "express";
-import { createCategory } from "../controller/categotyController.js";
+import {
+  allCategory,
+  createCategory,
+  deleteCategory,
+  getCategory,
+  updateCategory,
+} from "../controller/categotyController.js";
 import authMidellware, { isAdmin } from "../midellwares/authMidellware.js";
 
 export const router = Express.Router();
 
 router.post("/", authMidellware, isAdmin, createCategory);
+router.put("/update-category/:id", authMidellware, isAdmin, updateCategory);
+router.get("/search-category/:id", getCategory);
+router.get("/", allCategory);
+router.delete("/delete-category/:id", authMidellware, isAdmin, deleteCategory);
